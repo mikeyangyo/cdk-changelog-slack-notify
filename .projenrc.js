b51cd48c77d9f357f6700fab5584beef859e99aa
MikeYang <mike@gdp.world>
2020-12-03 12:15:29 +0800

    feat: support cdk 1.74.0

--- .projenrc.js
+++ .projenrc.js
@@ -1,50 +1,50 @@
-const { AwsCdkConstructLibrary } = require("projen");
+const { AwsCdkConstructLibrary } = require('projen');
 
-const AWS_CDK_LATEST_RELEASE = "1.74.0";
-const PROJECT_NAME = "cdk-changelog-slack-notify";
-const MODULE_NAME = "cdk_changelog_slack_notify";
+const AWS_CDK_LATEST_RELEASE = '1.74.0';
+const PROJECT_NAME = 'cdk-changelog-slack-notify';
+const MODULE_NAME = 'cdk_changelog_slack_notify';
 const PROJECT_DESCRIPTION =
-	"A JSII construct lib to deploy a service to send new changes pushed to codecommit to slack channel";
+	'A JSII construct lib to deploy a service to send new changes pushed to codecommit to slack channel';
 
 const project = new AwsCdkConstructLibrary({
-	authorAddress: "perryvm06vm06@gmail.com",
-	authorName: "mikeyangyo",
-	cdkVersion: AWS_CDK_LATEST_RELEASE,
-	name: PROJECT_NAME,
-	repository: "https://github.com/mikeyangyo/cdk-changelog-slack-notify.git",
-	description: PROJECT_DESCRIPTION,
-	license: "MIT",
-	deps: [
-		"@aws-cdk/core",
-		"@aws-cdk/aws-codecommit",
-		"@aws-cdk/aws-iam",
-		"@aws-cdk/aws-lambda",
-		"@aws-cdk/aws-events-targets",
-	],
-	peerDeps: [
-		"@aws-cdk/core",
-		"@aws-cdk/aws-codecommit",
-		"@aws-cdk/aws-iam",
-		"@aws-cdk/aws-lambda",
-		"@aws-cdk/aws-events-targets",
-	],
-	keywords: ["aws", "cdk", "codecommit", "slack", "changelog"],
-	python: {
-		distName: PROJECT_NAME,
-		module: MODULE_NAME,
-	},
-  defaultReleaseBranch: "main",
+  authorAddress: 'perryvm06vm06@gmail.com',
+  authorName: 'mikeyangyo',
+  cdkVersion: AWS_CDK_LATEST_RELEASE,
+  name: PROJECT_NAME,
+  repository: 'https://github.com/mikeyangyo/cdk-changelog-slack-notify.git',
+  description: PROJECT_DESCRIPTION,
+  license: 'MIT',
+  deps: [
+    `@aws-cdk/core@${AWS_CDK_LATEST_RELEASE}`,
+    `@aws-cdk/aws-codecommit@${AWS_CDK_LATEST_RELEASE}`,
+    `@aws-cdk/aws-iam@${AWS_CDK_LATEST_RELEASE}`,
+    `@aws-cdk/aws-lambda@${AWS_CDK_LATEST_RELEASE}`,
+    `@aws-cdk/aws-events-targets@${AWS_CDK_LATEST_RELEASE}`,
+  ],
+  peerDeps: [
+    `@aws-cdk/core@${AWS_CDK_LATEST_RELEASE}`,
+    `@aws-cdk/aws-codecommit@${AWS_CDK_LATEST_RELEASE}`,
+    `@aws-cdk/aws-iam@${AWS_CDK_LATEST_RELEASE}`,
+    `@aws-cdk/aws-lambda@${AWS_CDK_LATEST_RELEASE}`,
+    `@aws-cdk/aws-events-targets@${AWS_CDK_LATEST_RELEASE}`,
+  ],
+  keywords: ['aws', 'cdk', 'codecommit', 'slack', 'changelog'],
+  python: {
+    distName: PROJECT_NAME,
+    module: MODULE_NAME,
+  },
+  defaultReleaseBranch: 'main',
   codeCov: true,
-  codeCovTokenSecret: 'CODECOV_TOKEN'
+  codeCovTokenSecret: 'CODECOV_TOKEN',
 });
 
 const common_exclude = [
-	"cdk.out",
-	"cdk.context.json",
-	"yarn-error.log",
-	".env",
-	".vscode",
-	"images"
+  'cdk.out',
+  'cdk.context.json',
+  'yarn-error.log',
+  '.env',
+  '.vscode',
+  'images',
 ];
 project.npmignore.exclude(...common_exclude);
 project.gitignore.exclude(...common_exclude);
